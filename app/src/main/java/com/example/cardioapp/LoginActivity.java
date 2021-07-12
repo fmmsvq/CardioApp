0866f517e1762b89d9ff5f24d9450499b6b26cd3
FATIMA <fmm.svq@gmail.com>
2021-07-12 20:59:22 +0200

    Diseño de Login arreglado

---
+++ app/src/main/java/com/example/cardioapp/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.example.cardioapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        //Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+    }
+
+}
--- app/src/main/java/com/example/cardioapp/MainActivity.java
+++ app/src/main/java/com/example/cardioapp/MainActivity.java
@@ -1,18 +1,29 @@
 package com.example.cardioapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
-import com.example.cardioapp.ui.login.LoginActivity;
+import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.cardioapp.LoginActivity;
+/** Un Intent es un objeto que proporciona vinculación en tiempo de ejecución entre componentes separados, como dos actividades. El Intent representa la intención que tiene una app de realizar una tarea. Puedes usar intents para varias tareas; pero, en esta lección, tu intent inicia otra actividad.
+ * */
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+
+        // Arranca la siguiente actvidad pasados unos segundos
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                Intent mInHome = new Intent(MainActivity.this, LoginActivity.class);
+                MainActivity.this.startActivity(mInHome);
+                MainActivity.this.finish();
+            }
+        }, 3000);
     }
 }
