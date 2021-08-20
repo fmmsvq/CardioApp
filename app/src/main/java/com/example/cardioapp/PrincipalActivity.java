b7534a3038b0a31b325519bf4939f3fb08d0451a
FATIMA <fmm.svq@gmail.com>
2021-08-20 22:34:06 +0200

    Añadido Menu lateral

--- app/src/main/java/com/example/cardioapp/PrincipalActivity.java
+++ app/src/main/java/com/example/cardioapp/PrincipalActivity.java
@@ -1,6 +1,8 @@
 package com.example.cardioapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 
@@ -10,5 +12,10 @@ public class PrincipalActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+
+    // Menu lateral
+        final DrawerLayout drawerLayout = findViewById(R.id.drawerlayout);
+        findViewById(R.id.btnmenu).setOnClickListener(v -> drawerLayout.openDrawer(GravityCompat.START));
+
     }
 }
